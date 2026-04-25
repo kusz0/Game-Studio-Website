209ae52e59910d1f706466581d390272565054ec
Kacper Kuszek <kuszekkacper@gmail.com>
2026-04-25 02:17:05 +0200

    init

---
+++ scripts/main.js
@@ -0,0 +1,20 @@
+const videos = [
+  "../videos/water.mp4",
+  "../videos/Heven.mp4",
+  "../videos/Bomba.mp4",
+  "../videos/Death.mp4"
+];
+
+let index = 0;
+const video = document.getElementById("bgVideo");
+
+function playNextVideo() {
+  video.src = videos[index];
+  video.play();
+  index = (index + 1) % videos.length;
+}
+
+video.addEventListener("ended", playNextVideo);
+
+// start pierwszego
+playNextVideo();
